4a0d4d4fd00e2efb9da55490ccd581fb4791ff0e
sylvia-ya <1110509@chwjh.tp.edu.tw>
2025-02-18 15:55:34 +0800

    frist

---
+++ NO.1/Form1.cs
@@ -0,0 +1,26 @@
+namespace NO._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "ÂI§Ú·F¹À";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
